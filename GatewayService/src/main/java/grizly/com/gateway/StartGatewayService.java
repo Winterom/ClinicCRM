010665d068d4bc4c5d1e63f9fa914ad3c4b125b1
Winterom <winterom@gmail.com>
2022-03-23 10:36:46 +0300

    Initial commit

---
+++ GatewayService/src/main/java/grizly/com/gateway/StartGatewayService.java
@@ -0,0 +1,11 @@
+package grizly.com.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartGatewayService {
+    public static void main(String[] args) {
+        SpringApplication.run(StartGatewayService.class,args);
+    }
+}
